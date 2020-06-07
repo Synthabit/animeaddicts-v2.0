431a1ae64a1097c9a820c74fd9f9dc7b478e3be6
Synthabit <jeremymichevans@gmail.com>
2020-06-07 13:33:02 -0400

    Added an image gallery; need to add ability to display image info when clicked.

--- scripts/templates/nav.js
+++ scripts/templates/nav.js
@@ -1,5 +1,5 @@
 // Consider: This will not work in internet explorer.
-var date = "6-4-2020 : 1"
+var date = "6-7-2020 : 1"
 var NAV = NAV || (function() {
     var _args = {}; // private
     return {
